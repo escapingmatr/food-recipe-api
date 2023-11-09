4fbe0b01f59d01b78a44646ff1e4fbd034ac9ca4
escapingmatr <js.scott.park@gmail.com>
2023-11-09 05:23:05 -0500

    handling post and get request

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,12 +6,13 @@ const db = require('../db'); // Import your PostgreSQL database connection
 router.post('/', async (req, res) => {
   try {
     // Extract data from the request body
-    const { name, ingredients, instructions } = req.body;
+    const { id, name, description, ingredients, instructions, pictures } =
+      req.body;
 
     // Insert the new recipe into the database
     const newRecipe = await db.one(
-      'INSERT INTO recipes (name, ingredients, instructions) VALUES ($1, $2, $3) RETURNING *',
-      [name, ingredients, instructions]
+      'INSERT INTO recipes (id, name, description, ingredients, instructions, pictures) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      [id, name, description, ingredients, instructions, pictures]
     );
 
     res.status(201).json(newRecipe);
@@ -21,6 +22,36 @@ router.post('/', async (req, res) => {
       .status(500)
       .json({ error: 'An error occurred while creating the recipe.' });
   }
+});
+
+// Retrieve a single recipe by ID
+router.get('/:recipeId', async (req, res) => {
+  try {
+    const { recipeId } = req.params;
+
+    // Fetch a single recipe by ID from the database
+    const recipe = await db.one(
+      'SELECT * FROM recipes WHERE id = $1',
+      recipeId
+    );
+
+    res.json(recipe);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: 'An error occurred while fetching the recipe.' });
+  }
+});
+
+// Update a recipe by ID
+router.put('/:recipeId', async (req, res) => {
+  // Implement update logic here
+});
+
+// Delete a recipe by ID
+router.delete('/:recipeId', async (req, res) => {
+  // Implement delete logic here
 });
 
 // Define other routes for reading, updating, and deleting recipes
